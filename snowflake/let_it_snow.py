d028b2c506b9942c9c2325815ac1dcfd2fce65d8
jayanams <jayana99ms@gmail.com>
2022-12-05 00:16:02 +0100

    2st update

--- snowflake/let_it_snow.py
+++ snowflake/let_it_snow.py
@@ -6,15 +6,14 @@ def main(speed=0, bg_color="grey"):
     # create Turtle object
     turtle_screen = turtle.Screen()
     myTurtle = turtle.Turtle()
-    
+
     # set speed to 'fastest = 0'
     myTurtle.speed(speed)
     # change background color
     turtle_screen.bgcolor(bg_color)
-  
- 
-    """TODO: define different colors here"""
 
+    colormode(255)
+    """TODO: define different colors here"""
 
     for _ in range(10):
         # define some params
@@ -22,7 +21,7 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
-
+        myTurtle.color(randint(0, 255), randint(0, 255), randint(0, 255))
         # Go to the start position of the snowflake
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
